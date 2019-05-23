ca82e60059a805e46d74be2e443e240bcb5bd50a
Michael Thomsen <m@svth.dk>
2019-05-23 14:45:09 +0200

    Stub out app

---
+++ lib/rallycards.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class RallyAlertCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Text('RallyAlertCard placeholder'),
+    );
+  }
+}
+
+class RallyAccountsCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Text('RallyAccountsCard placeholder'),
+    );
+  }
+}
+
+class RallyBillsCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Text('RallyBillsCard placeholder'),
+    );
+  }
+}
